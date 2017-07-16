0d203372695a5efc150bdd5518078ff8133265ae
Evan Gibbs <evanegibbs@gmail.com>
2017-07-16 15:54:49 -0700

    Added Level intro, control screen. About to start sounds

---
+++ Assets/Scripts/ButtonCheck.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonCheck : MonoBehaviour {
+
+	public BlackMask blackMask;
+
+	void Update () {
+		if (Input.GetButtonDown("Jump")) {
+			blackMask.ExitLevel();
+		}
+	}
+}
---
+++ Assets/Scripts/LevelIntro.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelIntro : MonoBehaviour {
+
+	public int levelNumber = 0;
+	Animator animator;
+
+	void Start () {
+		animator = GetComponent<Animator>();
+		if (levelNumber == 1) {
+			animator.SetTrigger("Level1");
+		}
+		if (levelNumber == 2) {
+			animator.SetTrigger("Level2");
+		}
+		if (levelNumber == 3) {
+			animator.SetTrigger("Level3");
+		}
+		if (levelNumber == 4) {
+			animator.SetTrigger("Level4");
+		}
+	}
+
+	public void DestroySelf() {
+		Destroy(gameObject);
+	}
+}
